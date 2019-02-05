d8a0ffa98643d2bc35f5964badd4ece2e52396f0
Steve <Steve@SteveS-iMAC27.local>
2019-02-05 11:56:14 -0700

    Added the beginning of the random number generator

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+    @IBOutlet weak var diceimageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func rollBtn1(_ sender: UIButton) {
+        //This is the block of code that will get executed after button press
+        
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
 
+        print(randomDiceIndex1)
+        print(randomDiceIndex2)
+    }
+    
 }
 
